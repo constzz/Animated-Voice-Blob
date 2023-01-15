52aa2a7fd38572af4933cfefd622d8eee697d6b2
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2023-01-15 11:53:15 +0200

    Initial Commit

---
+++ AnimatedVoiceBlob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedVoiceBlob
+//
+//  Created by Konstantin Bezzemelnyi on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
